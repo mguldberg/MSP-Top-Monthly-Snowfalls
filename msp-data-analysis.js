5f6335c9fe4f7ca2c6b9d7b958ad7c0edd0021a8
mguldberg <guldberg@sbcglobal.net>
2019-03-07 21:33:55 -0600

    next udpate, added promise

--- msp-data-analysis.js
+++ msp-data-analysis.js
@@ -1,7 +1,6 @@
 // Load the NPM Package mysql
 var mysql = require("mysql");
-
-
+const moment = require("moment")
 var fs = require('fs');
 
 
@@ -23,29 +22,42 @@ var queryStringCount = "select count(*) as count from msp_snow_data.80_89_msp_da
 connection.query(queryStringCount, (err, result) => {
     queryStringCountResult = result[0].count;
     console.log('queryStringCountResult', queryStringCountResult);
-    queryStringCountResult = 40;
+    // queryStringCountResult = 5;
 
-    var totalArray = [];
-    var filteredSum = [];
-    for (i = 1; i <= queryStringCountResult; i++) {
-        var arraySum = [];
-
-        sqlStringFunction(i, (i + 29), function (err, res) {
-
-            arraySum = res.map(data => data['Snow (inches)'])
-            filteredSum = arraySum.filter(data2 => data2 != 'T');
-            // console.log('result range ', arraySum);
-            totalArray.push({
-                'Start Date': res[0].Date,
-                'End Date': res[29].Date,
-                 Sum: filteredSum.reduce((total, num) => total+num)
-            });
-            console.log('totalArray', totalArray);
+    calcLoop(queryStringCountResult, 30)
+        .then(function (data) {
+            console.log(data);
+            return;
         })
-    }
-    connection.end();
+        .then(() => connection.end());
 
 })
+
+const calcLoop = (countOfRecords, numOfDays) => {
+    return new Promise(function (res, rej) {
+        var totalArray = [];
+        var filteredSum = [];
+
+        for (i = 1; i <= countOfRecords; i++) {
+            var arraySum = [];
+
+            sqlStringFunction(i, (i + numOfDays-1), function (err, res) {
+
+                arraySum = res.map(data => data['Snow (inches)']);
+                filteredSum = arraySum.filter(data2 => data2 != 'T');
+                // console.log('result range ', ...filteredSum);
+                // console.log('sum 2 things', (parseFloat(filteredSum[0])+parseFloat(filteredSum[1])));
+                // console.log('total sum',filteredSum.reduce((total, num) => total+parseFloat(num), 0));
+                totalArray.push({
+                    'Start Date': moment(res[0].Date).format("YYYY-MM-DD"),
+                    'End Date': moment(res[29].Date).format("YYYY-MM-DD"),
+                    Sum: filteredSum.reduce((total, num) => total + parseFloat(num), 0).toFixed(2)
+                });
+
+            })
+        }
+    })
+};
 // (console.log('result',result[0].count)));
 // (queryStringCountResult = result[0].count));
 
@@ -72,7 +84,7 @@ function sqlStringFunction(startIdx, endIdx, cb) {
         if (err) {
             throw err;
         }
-        console.log('result range ', result);
+        // console.log('result range ', result);
         cb(err, result);
     });
 };
